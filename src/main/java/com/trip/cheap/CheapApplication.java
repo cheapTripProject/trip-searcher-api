5d114c4bc7a0b74ef1cff95eedfaaebc3ea21365
ervin-maftei <ervmaftei5@gmail.com>
2023-08-07 23:18:24 +0300

    Initial commit

---
+++ src/main/java/com/trip/cheap/CheapApplication.java
@@ -0,0 +1,13 @@
+package com.trip.cheap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheapApplication.class, args);
+	}
+
+}
