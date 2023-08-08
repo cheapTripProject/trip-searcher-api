a2d3d6d983da07339aef07db24e503d56ebba43d
ervin-maftei <ervmaftei5@gmail.com>
2023-08-08 15:27:24 +0300

    added FlightQueryParam model

--- src/main/java/com/trip/cheap/flight/FlightQueryParam.java
+++ src/main/java/com/trip/cheap/flight/FlightQueryParam.java
@@ -4,7 +4,117 @@ public class FlightQueryParam {
 
     private String apiKey;
 
-
     private String flyFrom;
+
+    // dd/mm/yyyy
+    private String dateFrom;
+
+    // dd/mm/yyyy
+    private String dateTo;
+
+    // dd/mm/yyyy
+    private String returnFrom;
+
+    // dd/mm/yyyy
+    private String returnTo;
+
+    private int nightsInDstFrom;
+
+    private int nightsInDstTo;
+
+    // duration in hours
+    private int maxFlyDuration;
+
+    private boolean retFromDiffCity;
+
+    private boolean retToDiffCity;
+
+    private int oneForCity;
+
+    private int onePerDate;
+
+    private int adults;
+
+    private int children;
+
+    private int infants;
+
+    private String selectedCabins;
+
+    private String mixWithCabins;
+
+    private String adultHoldBag;
+
+    private String adultHandBag;
+
+    private String childHoldBag;
+
+    private String childHandBag;
+
+    private String flyDays;
+
+    private String flyDaysType;
+
+    private String retFlyDaysType;
+
+    private boolean onlyWorkingDays;
+
+    private boolean onlyWeekends;
+
+    private String partnerMarket;
+
+    private String curr;
+
+    private String locale;
+
+    private int priceFrom;
+
+    private int priceTo;
+
+    private String dtimeFrom;
+
+    private String dtimeTo;
+
+    private String atimeFrom;
+
+    private String atimeTo;
+
+    private String retDtimeFrom;
+
+    private String retDtimeTo;
+
+    private String retAtimeFrom;
+
+    private String retAtimeTo;
+
+    private String stopOverFrom;
+
+    private String stopOverTo;
+
+    private int maxStopOvers;
+
+    private int maxSectorStopOvers;
+
+    private int connOnDiffAirport;
+
+    private int retFromDiffAirport;
+
+    private int retToDiffAirport;
+
+    private String selectAirlines;
+
+    private boolean selectAirlinesExcluded;
+
+    private String selectStopAirport;
+
+    private boolean selectStopAirportExclude;
+
+    private String vehicleType;
+
+    private boolean enableVi;
+
+    private String sort;
+
+    private int limit;
 
 }
